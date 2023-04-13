8650020b87259e2d88e7902f011cf358d1115626
Santiago_Lacera <santiagoalacera@knights.ucf.edu>
2023-04-13 01:35:21 -0400

    Created 4 new commands

--- JS/commands.js
+++ JS/commands.js
@@ -1,12 +1,14 @@
 
 export const terminal = [
-    '<br><span style=\"color: #7b2869;\">User@Home:~$</span>'
+    '<br><span style=\"color: #7b2869;\">User@Home:~$ </span>'
 ]
 
 
 
 export const banner =
     [
+        "<br>" +
+        "<br>" +
         "                   .                                                                                                                  " + "<br>" +
         "                  / V\\                                                                                                                " + "<br>" +
         "              / `    /       ______                       _                            _                                          " + "<br>" +
@@ -26,6 +28,7 @@ export const banner =
 
 export const help = [
     "<br>" +
+    "<br>" +
     "    '<span style=\"color: #245953;\">about</span>'       '<span style=\"color: #ABC4AA;\">Learn a bit more about me</span>'" + "<br>" +
     "    '<span style=\"color: #245953;\">clear</span>'       '<span style=\"color: #ABC4AA;\">Clears the terminal</span>'" + "<br>" +
     "    '<span style=\"color: #245953;\">help</span>'        '<span style=\"color: #ABC4AA;\">Displays this help message</span>'" + "<br>" +
@@ -33,45 +36,87 @@ export const help = [
     "    '<span style=\"color: #245953;\">skills</span>'      '<span style=\"color: #ABC4AA;\">Displays my skills</span>'" + "<br>" +
     "    '<span style=\"color: #245953;\">projects</span>'    '<span style=\"color: #ABC4AA;\">Displays my projects</span>'" + "<br>" +
     "    '<span style=\"color: #245953;\">contact</span>'     '<span style=\"color: #ABC4AA;\">Displays my contact info</span>'" + "<br>" +
-    "    '<span style=\"color: #245953;\">resume</span>'      '<span style=\"color: #ABC4AA;\">Displays my resume</span>'" + "<br>"
+    "    '<span style=\"color: #245953;\">resume</span>'      '<span style=\"color: #ABC4AA;\">Redirects to my resume</span>'" + "<br>"
 ]
 
 export const about = [
+    "<br>" +
     "<span style=\"color: #ABC4AA;\">" +
     "<br>" +
-    "<span style=\"color: #F9E2AF;\">'</span>Hello, my name is Santiago Lacera and I'm an IT Undergrad at the University of Central Florida.<span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
-    "<span style=\"color: #F9E2AF;\">'</span>I have been interested in technology and development since I was kid, exploring this field ever since.<span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
-    "<span style=\"color: #F9E2AF;\">'</span>I am passionate about learning new things and staying up-to-date with the latest advancements in technology.<span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
-    "<span style=\"color: #F9E2AF;\">'</span>In addition to my technical skills, I have over 5 years of CS experience and 2+ years in IT-related roles<span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
-    "<span style=\"color: #F9E2AF;\">'</span>I am also bilingual in English and Spanish, which has proven to be a valuable asset in many of my previous jobs.<span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
-    "<span style=\"color: #F9E2AF;\">'</span>As an IT major, I have developed a strong foundation in programming and problem-solving.<span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
-    "<span style=\"color: #F9E2AF;\">'</span>I'm always looking for opportunities to apply these skills in real-world scenarios and looking for ways to improve<span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
-    "<span style=\"color: #F9E2AF;\">'</span>I am particularly interested in web development and cybersecurity, and I have completed several online courses.<span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
-    "<span style=\"color: #F9E2AF;\">'</span>Currently, I am looking for internships or part-time job for the Summer of 2023, where to develop my skills and experience.<span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
-    "<span style=\"color: #F9E2AF;\">'</span>If you have any opportunities that you think may be a good fit, please feel free to contact me!<span style=\"color: #F9E2AF;\"> Type --></span> <span style=\"color: #F9E2AF;\">'</span><span style=\"color: #245953;\">contact</span><span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
+    "  <span style=\"color: #F9E2AF;\">'</span>Hello, my name is Santiago Lacera and I'm an IT Undergrad at the University of Central Florida.<span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
+    "  <span style=\"color: #F9E2AF;\">'</span>I have been interested in technology and development since I was kid, exploring this field ever since.<span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
+    "  <span style=\"color: #F9E2AF;\">'</span>I am passionate about learning new things and staying up-to-date with the latest advancements in technology.<span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
+    "  <span style=\"color: #F9E2AF;\">'</span>In addition to my technical skills, I have over 5 years of CS experience and 2+ years in IT-related roles<span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
+    "  <span style=\"color: #F9E2AF;\">'</span>I am also bilingual in English and Spanish, which has proven to be a valuable asset in many of my previous jobs.<span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
+    "  <span style=\"color: #F9E2AF;\">'</span>As an IT major, I have developed a strong foundation in programming and problem-solving.<span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
+    "  <span style=\"color: #F9E2AF;\">'</span>I'm always looking for opportunities to apply these skills in real-world scenarios and looking for ways to improve<span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
+    "  <span style=\"color: #F9E2AF;\">'</span>I am particularly interested in web development and cybersecurity, and I have completed several online courses.<span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
+    "  <span style=\"color: #F9E2AF;\">'</span>Currently, I am looking for internships or part-time job for the Summer of 2023, where to develop my skills and experience.<span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
+    "  <span style=\"color: #F9E2AF;\">'</span>If you have any opportunities that you think may be a good fit, please feel free to contact me!<span style=\"color: #F9E2AF;\"> Type --></span> <span style=\"color: #F9E2AF;\">'</span><span style=\"color: #245953;\">contact</span><span style=\"color: #F9E2AF;\">'</span>" + "<br>" +
     "</span >"
 ]
 
 export const skills = [
+    "<br>" +
     "<span style=\"color: #ABC4AA;\">" +
     "<br>" +
-    "<span style=\"color: #F45050;\">**Programming Languages**</span>" + "<span style=\"color: #F45050;\">                         **Frameworks/Libraries**</span>" + "<br>" +
-    "     <span style=\"color: #F9E2AF;\"><</span>HTML/CSS/JS<span style=\"color: #F9E2AF;\">></span>" + "<span style=\"color: #F9E2AF;\">                                      <</span>React<span style=\"color: #F9E2AF;\">></span>" + "<br>" +
-    "     <span style=\"color: #F9E2AF;\"><</span>Java<span style=\"color: #F9E2AF;\">></span>" + "<span style=\"color: #F9E2AF;\">                                             <</span>Node<span style=\"color: #F9E2AF;\">></span>" + "<br>" +
-    "     <span style=\"color: #F9E2AF;\"><</span>Python<span style=\"color: #F9E2AF;\">></span>" + "<span style=\"color: #F9E2AF;\">                                           <</span>Express<span style=\"color: #F9E2AF;\">></span>" + "<br>" +
-    "     <span style=\"color: #F9E2AF;\"><</span>C<span style=\"color: #F9E2AF;\">></span>" + "<span style=\"color: #F9E2AF;\">                                                <</span>Bootstrap<span style=\"color: #F9E2AF;\">></span>" + "<br>" +
+    "  <span style=\"color: #F45050;\">**Programming Languages**</span>" + "<span style=\"color: #F45050;\">                         **Frameworks/Libraries**</span>" + "<br>" +
+    "       <span style=\"color: #F9E2AF;\"><</span>HTML/CSS/JS<span style=\"color: #F9E2AF;\">></span>" + "<span style=\"color: #F9E2AF;\">                                      <</span>React<span style=\"color: #F9E2AF;\">></span>" + "<br>" +
+    "       <span style=\"color: #F9E2AF;\"><</span>Java<span style=\"color: #F9E2AF;\">></span>" + "<span style=\"color: #F9E2AF;\">                                             <</span>Node<span style=\"color: #F9E2AF;\">></span>" + "<br>" +
+    "       <span style=\"color: #F9E2AF;\"><</span>Python<span style=\"color: #F9E2AF;\">></span>" + "<span style=\"color: #F9E2AF;\">                                           <</span>Express<span style=\"color: #F9E2AF;\">></span>" + "<br>" +
+    "       <span style=\"color: #F9E2AF;\"><</span>C<span style=\"color: #F9E2AF;\">></span>" + "<span style=\"color: #F9E2AF;\">                                                <</span>Bootstrap<span style=\"color: #F9E2AF;\">></span>" + "<br>" +
     "</span >"
 ]
 
 export const skills2 = [
     "<span style=\"color: #ABC4AA;\">" +
     "<br>" +
-    "<span style=\"color: #F45050;\">**Tools**</span>" + "<span style=\"color: #F45050;\">                                         **Databases**</span>" + "<br>" +
-    "     <span style=\"color: #F9E2AF;\"><</span>Git<span style=\"color: #F9E2AF;\">></span>" + "<span style=\"color: #F9E2AF;\">                                              <</span>MySQL<span style=\"color: #F9E2AF;\">></span>" + "<br>" +
-    "     <span style=\"color: #F9E2AF;\"><</span>VSCode<span style=\"color: #F9E2AF;\">></span>" + "<span style=\"color: #F9E2AF;\">                                           <</span>MongoDB<span style=\"color: #F9E2AF;\">></span>" + "<br>" +
-    "     <span style=\"color: #F9E2AF;\"><</span>GitHub<span style=\"color: #F9E2AF;\">></span>" + "<span style=\"color: #F9E2AF;\">                                           <</span>PostgreSQL<span style=\"color: #F9E2AF;\">></span>" + "<br>" +
-    "     <span style=\"color: #F9E2AF;\"><</span>SalesForce<span style=\"color: #F9E2AF;\">></span>" + "<span style=\"color: #F9E2AF;\">                                       <</span>SQLite<span style=\"color: #F9E2AF;\">></span>" + "<br>" +
+    "  <span style=\"color: #F45050;\">    **Tools**</span>" + "<span style=\"color: #F45050;\">                                         **Databases**</span>" + "<br>" +
+    "       <span style=\"color: #F9E2AF;\"><</span>Git<span style=\"color: #F9E2AF;\">></span>" + "<span style=\"color: #F9E2AF;\">                                              <</span>MySQL<span style=\"color: #F9E2AF;\">></span>" + "<br>" +
+    "       <span style=\"color: #F9E2AF;\"><</span>VSCode<span style=\"color: #F9E2AF;\">></span>" + "<span style=\"color: #F9E2AF;\">                                           <</span>MongoDB<span style=\"color: #F9E2AF;\">></span>" + "<br>" +
+    "       <span style=\"color: #F9E2AF;\"><</span>GitHub<span style=\"color: #F9E2AF;\">></span>" + "<span style=\"color: #F9E2AF;\">                                           <</span>PostgreSQL<span style=\"color: #F9E2AF;\">></span>" + "<br>" +
+    "       <span style=\"color: #F9E2AF;\"><</span>SalesForce<span style=\"color: #F9E2AF;\">></span>" + "<span style=\"color: #F9E2AF;\">                                       <</span>SQLite<span style=\"color: #F9E2AF;\">></span>" + "<br>" +
     "</span >"
+]
+
+export const projects = [
+    "<br>" +
+    "<span style=\"color: #ABC4AA;\">" +
+    "<br>" +
+    "       <span style=\"color: #F45050;\">**Projects**</span>" + "<br>" +
+    "       <span style=\"color: #F9E2AF;\"><</span><a href=\"https://santiagolacera.github.io/Portafolio-website/\" target=\"_blank\">Portfolio</a><span style=\"color: #F9E2AF;\">></span>" + "<br>" +
+    "       <span style=\"color: #F9E2AF;\"><</span><a href=\"https://santiagolacera.github.io/Bingo_JS/\" target=\"_blank\">Functional_Bingo_Game</a><span style=\"color: #F9E2AF;\">></span>" + "<br>" +
+    "       <span style=\"color: #F9E2AF;\"><</span><a href=\"https://santiagolacera.github.io/Company-Website-Template/\" target=\"_blank\">Template_for_a_company_website</a><span style=\"color: #F9E2AF;\">></span>" + "<br>" +
+    "       <span style=\"color: #F9E2AF;\"><</span><a href=\"https://santiagolacera.github.io/Valentine-Template-Phone.io/\" target=\"_blank\">Valentine_Template_website</a><span style=\"color: #F9E2AF;\">></span>" + "<br>" +
+    "</span >"
+]
+
+export const contact = [
+    "<br>" +
+    "<span style=\"color: #ABC4AA; \">" +
+    "<br>" +
+    "       <span style=\"color: #F45050;\">**Contact**</span>" + "<br>" +
+    "       <span style=\"color: #F9E2AF;\"><</span>LinkedIn<span style=\"color: #F9E2AF;\">></span>" + "   <span style=\"color: #F9E2AF;\">-></span>" + "    <a href=\"https://www.linkedin.com/in/santiago-lacera-03091999//\" target=\"_blank\">Santiago Lacera</a>" + "<br>" +
+    "       <span style=\"color: #F9E2AF;\"><</span>GitHub<span style=\"color: #F9E2AF;\">></span>" + "     <span style=\"color: #F9E2AF;\">-></span>" + "    <a href=\"https://github.com/SantiagoLacera/\" target=\"_blank\">Santiago_Lacera</a>" + "<br>" +
+    "       <span style=\"color: #F9E2AF;\"><</span>Email<span style=\"color: #F9E2AF;\">></span>" + "      <span style=\"color: #F9E2AF;\">-></span>" + "    <a href=\"mailto:santiagolacera251@gmail.com\" target=\"_blank\">Email Me </a>" + "<br>" +
+    "       <span style=\"color: #F9E2AF;\"><</span>Phone<span style=\"color: #F9E2AF;\">></span>" + "      <span style=\"color: #F9E2AF;\">-></span>" + "    <span style=\"color: #245953;\">407-460-5686</span>" + "<br>" +
+    "</span >"
+]
+
+
+export const cnf = [
+    "<br>" + "<br>" +
+    "<span style=\"color: #ABC4AA;\">" +
+    " Command not found. Type --help for a list of commands." +
+    "</span >" +
+    "<br>"
+]
+
+export const resume = [
+    "<br>" + "<br>" +
+    "<span style=\"color: #ABC4AA;\">" +
+    "    <a href=\"https://santiagolacera.github.io/Portafolio-website/resume.html\" target=\"_blank\">Redirecting to RESUME" + "<span style=\"color: #F9E2AF;\">.....</span>" + " </a>" +
+    "<br>"
 ]
 
 
