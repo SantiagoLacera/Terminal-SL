ec0bd61252c2126390739e8d639ad621db7a522e
Santiago_Lacera <santiagoalacera@knights.ucf.edu>
2023-04-13 01:34:49 -0400

    Function to redirect added & more options

--- JS/index.js
+++ JS/index.js
@@ -3,58 +3,85 @@ import {
     banner,
     help,
     skills, skills2,
-    terminal
+    terminal,
+    cnf,
+    projects,
+    contact,
+    resume
 } from './commands.js';
 
 let terminal_banner = document.getElementById('terminal-banner');
-let command_terminal = document.getElementById('command-terminal');
+let terminal_body = document.getElementById('terminal-body');
+let terminal_prebody = document.getElementById('terminal-prebody');
 let input = document.getElementById('command-input');
 let output = document.getElementById('command-prompt');
 let terminal_header = document.getElementById('terminal-header');
-
-$(terminal_header).after(terminal);
-$(document).ready(header(banner));
-
-
-
-
-
+let command_array = [];
+$(document).ready(function () {
+    header(banner)
+    //$(terminal_header).after(terminal);
 document.querySelector('#command-input').addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
         let command = input.value;
+        let res;
         switch (command) {
-            case "help":
-                output.innerHTML = help;
-                break;
             case "about":
-                output.innerHTML = about;
-                break;
-            case "skills":
-                output.innerHTML = skills + skills2;
+                res = about;
                 break;
             case "clear":
-                output.innerHTML = "";
+                res = "";
+                break;
+            case "help":
+                res = help;
                 break;
             case "banner":
-                output.innerHTML = banner;
+                res = banner;
                 break;
-            case "command":
-                output.innerHTML = terminal;
+            case "skills":
+                res = skills + skills2;
+                break;
+            case "projects":
+                res = projects;
+                break;
+            case "contact":
+                res = contact;
+                break;
+            case "resume":
+                res = resume;
+                newTab("https://santiagolacera.github.io/Portafolio-website/resume.html");
                 break;
             default:
-                output.innerHTML = "Command not found";
+                res = cnf;
                 break;
         }
-
-        output.innerHTML += terminal;
+        if (command == "clear") {
+            command_array = [];
+        }
+        else {
+            command_array.push(terminal);
+            command_array.push(command);
+            command_array.push(res);
+        }
+        input.value = "";
+        terminal_prebody.innerHTML = command_array.join('');
+        window.scrollTo(0, document.body.scrollHeight);
     }
 });
 
+});
 
 
 function header(banner) {
     terminal_banner.innerHTML = banner;
 }
 
+function newTab(link) {
+    setTimeout(function () {
+        window.open(link, "_blank");
+    }, 2000);
+}
 
-
+setTimeout(function () {
+    header(banner, "", 2000);
+    textarea.focus();
+}, 2000);
